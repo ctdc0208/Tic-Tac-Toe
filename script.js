5311f9a8e7912f57df15a5331766f6a322ad891d
nexus0208 <christiandelacruz0208@gmail.com>
2021-05-31 09:55:28 +0800

    add JS module and factory

---
+++ script.js
@@ -0,0 +1,17 @@
+let myGame = []; //gameboard
+let myPlayers = []; // players
+let gameFlow = []; // flow of the game
+
+const gameBoard = (() => {
+
+
+})(); // gameboard module
+
+const displayController = (() => {
+
+
+})(); // displayController module
+
+const players = (name) => {
+
+} // players factory
