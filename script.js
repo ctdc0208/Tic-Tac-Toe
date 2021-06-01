44e077866d2265117a4a5b3f7a01aa4e585a6681
nexus0208 <christiandelacruz0208@gmail.com>
2021-06-01 09:56:15 +0800

    add css player-marks

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let myPlayers = []; // players
 let gameFlow = []; // flow of the game
 
 const gameBoard = (() => {
-
+  function addMove() {
+    document.addEventListener('click', (event) => {
+      const { target } = event;
+      const tr = target.parentNode.parentNode.rowIndex - 1;
+      
+    }
+  );
+  }
 
 })(); // gameboard module
 
