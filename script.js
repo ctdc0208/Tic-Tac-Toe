c720e32a5df78511874b45c9b83fc4e88c051329
nexus0208 <christiandelacruz0208@gmail.com>
2021-06-02 08:52:40 +0800

    add myPlayers

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 let myGame = []; //gameboard
-let myPlayers = []; // players
+//let myPlayers = []; // players
 let gameFlow = []; // flow of the game
 
 const gameBoard = (() => {
-  function addMove() {
-    document.addEventListener('click', (event) => {
-      const { target } = event;
-      const tr = target.parentNode.parentNode.rowIndex - 1;
-      
     }
   );
   }
@@ -15,10 +10,34 @@ const gameBoard = (() => {
 })(); // gameboard module
 
 const displayController = (() => {
+  function addMove() {
+    document.addEventListener('click', (event) => {
+      const { target } = event;
+      const tr = target.parentNode.parentNode.rowIndex - 1;
 
+    }
+  );
+  }
 
 })(); // displayController module
 
-const players = (name) => {
+const myPlayers = (playerOne, playerTwo) => {
+  function createPlayer(event) {
+    const form = document.querySelector('form');
+    const playerOneInput = document.querySelector('#player-one');
+    const playerTwoInput = document.querySelector('#player-two');
 
+    if (playerOneInput.value !== '' && playerOneInput.value !== '') {
+      myPlayers(playerOneInput.value, playerTwoInput.value)
+      }
+    form.reset();
+    }
+
+  return {playerOne, playerTwo};
 } // players factory
+
+
+const jeff = myPlayers("Jeff", "Jeff2");
+
+console.log(jeff.playerOne);
+console.log(jeff.playerTwo);
