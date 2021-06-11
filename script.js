94c4ab2bfb25b1c409664aa49481e243b43680eb
nexus0208 <christiandelacruz0208@gmail.com>
2021-06-11 11:50:50 +0800

    put render_board on a module

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let gameboard = ["", "", "", "", "", "", "", "", ""];
+let game_board = ["", "", "", "", "", "", "", "", ""];
 let myPlayers = [];
 let gameFlow = [];
 
-const game_board = () => {
-  const board_container = document.querySelector(".play-area");
-  board_container.innerHTML = ""
-  gameboard.forEach((e, i) => {
-    board_container.innerHTML += `<div id="block_${i}" class="block" onclick="addPlayerMove(${i})">${game_board[i]}</div>`
-    if (e == playerOne || e == playerTwo) {
-      document.querySelector(`#block_${i}`).classList.add("occupied");
-    }
-  });
-};
 
-game_board();
+const rendergame = (() => {
+  const render_board = () => {
+    const board_container = document.querySelector(".play-area");
+    board_container.innerHTML = ""
+    game_board.forEach((e, i) => {
+        board_container.innerHTML += `<div id="block_${i}" class="block" onclick="addPlayerMove(${i})">${game_board[i]}</div>`
+      if (e == playerOne || e == playerTwo) {
+        document.querySelector(`#block_${i}`).classList.add("occupied");
+      }
+    });
+  };
+  render_board();
+});
+
+rendergame();
